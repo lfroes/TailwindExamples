5a9159376078ccd521eabeadc247e909a79e6c9e
CykaFroes <nikowluiz@gmail.com>
2022-04-21 11:28:22 -0300

    Updated next js only

--- without-tailwind-next/pages/index.jsx
+++ without-tailwind-next/pages/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useTheme } from "next-themes";
 
@@ -18,51 +19,56 @@ export default function Home() {
   }, [darkMode]);
 
   return (
-    <div className={styles.main}>
-      <div className={styles.navigation}>
-        <div className={styles.title}>TRNDY</div>
-        <div className={styles.togglerContainer}>
-          <div className={styles.toggler}>
-            <label htmlFor="toggle" className={styles.toggleLabel}>
-              <div className={styles.relative}>
-                <input
-                  type="checkbox"
-                  id="toggle"
-                  className={styles.srOnly}
-                  value={darkMode}
-                  onChange={() => setMode(!darkMode)}
-                />
-                <div className={styles.toggleOuter}></div>
-                <div className={`${styles.toggleInner} dot`}></div>
-              </div>
-              <div className={styles.togglerText}>
-                {!darkMode ? "Light Mode" : "Dark Mode"}
-              </div>
-            </label>
+    <>
+      <Head>
+        <title>Next Js Only</title>
+      </Head>
+      <div className={styles.main}>
+        <div className={styles.navigation}>
+          <div className={styles.title}>TRNDY</div>
+          <div className={styles.togglerContainer}>
+            <div className={styles.toggler}>
+              <label htmlFor="toggle" className={styles.toggleLabel}>
+                <div className={styles.relative}>
+                  <input
+                    type="checkbox"
+                    id="toggle"
+                    className={styles.srOnly}
+                    value={darkMode}
+                    onChange={() => setMode(!darkMode)}
+                  />
+                  <div className={styles.toggleOuter}></div>
+                  <div className={`${styles.toggleInner} dot`}></div>
+                </div>
+                <div className={styles.togglerText}>
+                  {!darkMode ? "Light Mode" : "Dark Mode"}
+                </div>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className={styles.mainContent}>
+          <div className={styles.mainBanner}>
+            <Image
+              src={mainBanner}
+              alt="main-banner"
+              width={2500}
+              height={1400}
+            />
+          </div>
+          <div className={styles.mainInfo}>
+            <h1 className={styles.mainTitle}>
+              Óculos novos em um estalar de dedos!
+            </h1>
+            <p className={styles.mainSubTitle}>
+              Compre óculos dos mais variados com a TRNDY
+            </p>
+            <div className={styles.CallToAction}>
+              <button className={styles.ctaButton}>Peça Agora</button>
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles.mainContent}>
-        <div className={styles.mainBanner}>
-          <Image
-            src={mainBanner}
-            alt="main-banner"
-            width={2500}
-            height={1400}
-          />
-        </div>
-        <div className={styles.mainInfo}>
-          <h1 className={styles.mainTitle}>
-            Óculos novos em um estalar de dedos!
-          </h1>
-          <p className={styles.mainSubTitle}>
-            Compre óculos dos mais variados com a TRNDY
-          </p>
-          <div className={styles.CallToAction}>
-            <button className={styles.ctaButton}>Peça Agora</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
