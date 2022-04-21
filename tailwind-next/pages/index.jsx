a791d3bab503b10d17fa22647706d4d62dff5ea2
CykaFroes <nikowluiz@gmail.com>
2022-04-21 01:51:29 -0300

    Added without tailwind - Next Js

--- tailwind-next/pages/index.jsx
+++ tailwind-next/pages/index.jsx
@@ -30,7 +30,7 @@ export default function Home() {
                 <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
                 <div className="dot absolute left-1 top-1 bg-trndy-secondary dark:bg-trndy-primary w-6 h-6 rounded-full transition"></div>
               </div>
-              <div class="ml-3 text-gray-700 dark:text-trndy-primary font-medium">{!darkMode ? "Light Mode" : "Dark Mode"}</div>
+              <div className="ml-3 text-gray-700 dark:text-trndy-primary font-medium">{!darkMode ? "Light Mode" : "Dark Mode"}</div>
             </label>
           </div>
         </div>
