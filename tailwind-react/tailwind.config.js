9259e976e02aeb4838cf61015d39e56eda370214
CykaFroes <nikowluiz@gmail.com>
2022-04-27 16:24:24 -0300

    Finished React Without tailwind

---
+++ tailwind-react/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        "trndy-primary": "#F9F1E6",
+        "trndy-secondary": "#AE3929",
+        "trndy-gray": "#6B6463",
+        "trndy-brown": "#D87E24"
+      },
+      fontFamily: {
+        "montserrat": ["Montserrat", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
